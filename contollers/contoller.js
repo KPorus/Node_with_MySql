07dd8c8f4615552aabab6cec53360494d52d5078
KPorus <designerfardin2021@gmail.com>
2023-05-08 22:58:42 +0600

    controller add

--- contollers/contoller.js
+++ contollers/contoller.js
@@ -1,3 +1,36 @@
 const db = require("../models");
 
-db.users;
+const user = db.users;
+
+exports.adduser = async (req, res) => {
+  let user = {
+    name: req.body.name,
+    email: req.body.email,
+    pass: req.body.pass,
+  };
+
+  let result = await user.create(user);
+  res.status(200).json({
+    status: "success",
+    result,
+  });
+};
+
+exports.getAllusers = async (req, res)=>{
+    let users = await user.findAll({
+        attributes: [
+            'name','email'
+        ]
+    });
+    console.log(users);
+    if(users)
+    {
+        res.status(200).json({
+          status: "success",
+          users,
+        });
+    }
+    else{
+        res.status(404).json({  status: "error", message: "users not found" });
+    }
+}
