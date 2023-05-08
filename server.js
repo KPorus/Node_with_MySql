1917e348b57e1ebb17ebfd50fd4e8e04d25e5c11
KPorus <designerfardin2021@gmail.com>
2023-05-08 22:47:05 +0600

    add env

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 8000;
@@ -15,11 +16,10 @@ app.use(
   express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 1000000 })
 );
 
-app.get('/', (req, res) =>{
-    res.send("I am watching you");
-})
+app.get("/", (req, res) => {
+  res.send("I am watching you");
+});
 
 app.listen(port, () => {
   console.log(`server is runnning at ${port}`);
 });
-
